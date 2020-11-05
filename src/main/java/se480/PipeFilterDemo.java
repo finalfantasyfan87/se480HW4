0f8f79467010be9fec3c997a643868cfc80a4a15
Jenni Hay <finalfantasyguru8705@gmail.com>
2020-11-05 05:01:45 -0600

    spring boot dependency for a creating executable jar

--- src/main/java/se480/PipeFilterDemo.java
+++ src/main/java/se480/PipeFilterDemo.java
@@ -11,10 +11,16 @@ public class PipeFilterDemo {
 
     public static void main(String[] args) throws IOException {
         StopwordsPipe stopwordsPipe = new StopwordsPipe();
+        //running from the command line or IDE, ADD THE FILE NAME as
+        //a program argument
         List words = stopwordsPipe.convertFileToListOfWords(args[0]);
+        //update the list of words by removing all stopwords
         List updatedList = stopwordsPipe.removeStopWordsPushWordCounter(words);
+
+        //call the nonAlphaPipeRemover to remove all nonAlpha characters
         NonAlphaRemoverPipe nonAlphaRemover = new NonAlphaRemoverPipe();
         List alphaChars = nonAlphaRemover.removeNonAlphaChars(updatedList);
+
         nonAlphaRemover.writeToFile(alphaChars);
         nonAlphaRemover.callStemmerPipe();
     }
